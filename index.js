3fd7c8ad0d0d85bda0e792c48507e04a887059fe
unknown <giovanne.oliv@gmail.com>
2022-12-02 21:05:14 -0400

    Second commit

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const routes = require('./routes/routes');
 const app = express()
 const porta = 3000;
-const cors = require('cors')
+const cors = require('cors');
+const middlewareValidaJwt = require('./validarJwt');
+const AuthController = require('./controllers/AuthController');
+
+
 // ...
 app.use(cors())
 
@@ -11,8 +15,8 @@ app.use(cors())
 app.use(express.json())
 
 
-
-app.use('/',routes)
+app.post('/login',AuthController.login)
+app.use('/',middlewareValidaJwt,routes)
 app.listen(porta,()=>{
     console.log('Servidor Iniciado com sucesso')
 })
---
+++ seeders/20221203002107-User.js
@@ -0,0 +1,21 @@
+'use strict';
+const { User } = require('../models')
+const bcrypt = require('bcrypt')
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash('admin',salt)
+    await User.create({
+      name: 'Administrador',
+      email: 'admin@admin.com',
+      password: passwordHash
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await User.findOne({ where: {
+      email: 'admin@admin.com'
+    }}).destroy()
+  }
+};
